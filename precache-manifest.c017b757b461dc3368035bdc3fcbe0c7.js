461f49fb6d17f8ce0f42cf3a1aed6eb7bb87f442
studioMac-116 <studiomac-116@studioMac-116s-iMac.local>
2022-02-09 10:21:23 +0530

    Updates

---
+++ precache-manifest.c017b757b461dc3368035bdc3fcbe0c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f391517ce51782aa748358dac94f8e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dddd66765ea0fa91efa1",
+    "url": "/monsters-rolodex/static/css/main.89cfe6dc.chunk.css"
+  },
+  {
+    "revision": "b004dedba530bda1a65d",
+    "url": "/monsters-rolodex/static/js/2.5d35cf58.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/monsters-rolodex/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "dddd66765ea0fa91efa1",
+    "url": "/monsters-rolodex/static/js/main.827c2cf8.chunk.js"
+  },
+  {
+    "revision": "6c81d719dabae79c8732",
+    "url": "/monsters-rolodex/static/js/runtime~main.89d7aea0.js"
+  }
+]);
